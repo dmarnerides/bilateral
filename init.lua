e36d149dcd663a84ed4ab1723b8fccc556c8cbb4
Demetris <dmarnerides@gmail.com>
2017-06-02 13:13:03 +0100

    fixed single argument

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local type2tensor = { ["torch.FloatTensor"] = torch.FloatTensor,
 
 local function filter(img,edge,sigma_s,sigma_r)
     require 'libfilter'
+    edge = edge or img
     local w,h = img:size(2), img:size(1)
     sigma_s = sigma_s or math.min(w,h)/60
     sigma_r = sigma_r or (img:max() - img:min())*0.1
